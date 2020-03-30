dd2e7291195e7251dc8f78c5d3c6bca5c2c90a48
Dylan <dylan.ebert@gmail.com>
2020-03-29 20:32:43 -0400

    .

--- code/experiments/runNBC.bat
+++
@@ -1,8 +0,0 @@
-SET PROJHOME=C:\Users\dylan\Documents\matlab\bparhmm\code\
-SET JOB_ID=%1
-SET TASK_ID=%2
-SET DATASET=%3
-SET PREFIX=cd %PROJHOME%; addpath(genpath('.'));
-SET CMD=runNBC %JOB_ID% %TASK_ID% %DATASET%;
-cd %PROJHOME%
-matlab -nojvm -nodesktop -nodisplay -nosplash -r "%PREFIX% %CMD% exit;"
--- runNBCJobs.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\dylan\Documents\matlab\bparhmm\code\experiments\runNBC.bat 2 1 nbc_5hz
-C:\Users\dylan\Documents\matlab\bparhmm\code\experiments\runNBC.bat 3 1 nbc_10hz
